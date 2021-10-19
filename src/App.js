4ffe0e45c6e5ac09cf41d9d2b51dd7b68225c3d7
JustinH0404 <justin.hong0404@gmail.com>
2021-10-19 10:25:15 -0700

    revert personal website

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
     <NavBar />
     <Switch>
-      <Route exact path="/">
+      <Route exact path="/personal-website">
         <HomePages /> 
         <Projects /> 
       </Route>
