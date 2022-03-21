ee4469065de1c48a93d1fae1a308cd99ad764743
Justin Hong <justin0404@berkeley.edu>
2022-03-20 23:48:53 -0700

    added projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -10,47 +10,32 @@ export default function Projects() {
   AOS.init({
     duration:1000
 });
-// const [Projects, setProjects] = React.useState(null);
-//   async function fetData(){
-//     const res = await axios.get(
-//       "https://api.airtable.com/v0/appKlfLPKeXS4jX0p/Project",
-//       {
-//         headers: {
-//           authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
-//         }
-//       }
-//     )
-//     setProjects(res.data.records)
-//     console.log(res.data.records)
-//   }
-//   React.useEffect(() => {
-//     fetData();
-//   }, []);
-//   if(!Projects){
-//     return (
-//       <p> Loading </p>
-//     )
-//   }
+const [Projects, setProjects] = React.useState(null);
+  async function fetData(){
+    const res = await axios.get(
+      "https://api.airtable.com/v0/appKlfLPKeXS4jX0p/Project",
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        }
+      }
+    )
+    setProjects(res.data.records)
+    console.log(res.data.records)
+  }
+  React.useEffect(() => {
+    fetData();
+  }, []);
+  if(!Projects){
+    return (
+      <p> Loading </p>
+    )
+  }
     return (
     <section id = "projectsPage">
       <div class = "projectsPage">
         <p class = "projectPageTitle" data-aos = "fade-down">Projects</p>
-        <div class = "container projectm projectBorder">
-          <div class = "row">
-            <div class = "col-4" data-aos = "fade-right">
-              <img class = "projectPicture" src = {proj1} alt = ""/>
-            </div>
-            <div class = "col-8" data-aos = "fade-right">
-              <div class = "projectTitle">p5 Project</div>
-              <div class = "projectAuthors">Justin Hong</div>
-              <div class = "projectDescription">This was my very first project coding project! Drawing on inspiration from 
-              the Ridge Rubik's Cube Club, which I was a part of in High School, I decided to create a project that created
-              a step by step process on solving a Rubik's Cube. To do this, I learned how to use JavaScript, a little bit of React,
-              and the p5 library.</div>
-            </div>
-          </div>
-        </div>
-        {/* {
+        {
           Projects.map((record) => (
             <Project
             name = {record.fields.Name}
@@ -60,7 +45,7 @@ export default function Projects() {
 
             </Project>
           ))
-        } */}
+        }
       </div>
     </section>
     );
